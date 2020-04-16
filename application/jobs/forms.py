868d544399b63697842cd1659ab11460128dff3e
nicholsss <mickyaw1995@gmail.com>
2020-04-16 18:59:07 +0300

    bootstrap styling

--- application/jobs/forms.py
+++ application/jobs/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators,IntegerField
+from wtforms import StringField, validators,IntegerField,TextAreaField
 
 class JobForm(FlaskForm):
     name = StringField("Job name", [validators.Length(min=3,max=12)])
     salary = IntegerField("Salary")
-    description = StringField('Job Description', [validators.Length(max=600)])
+    description = TextAreaField('Job Description', [validators.Length(max=600)])
   
     class Meta:
         csrf = False
