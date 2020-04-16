350e5476fdebba9869e1d91838becb14c729370d
nicholsss <mickyaw1995@gmail.com>
2020-04-16 19:16:29 +0300

    Bootstrap style

--- application/jobs/forms.py
+++ application/jobs/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, validators,IntegerField,TextAreaField
 
 class JobForm(FlaskForm):
     name = StringField("Job name", [validators.Length(min=3,max=12)])
-    salary = IntegerField("Salary")
+    salary = IntegerField("Salary",[validators.NumberRange(max=1000000)])
     description = TextAreaField('Job Description', [validators.Length(max=600)])
   
     class Meta:
